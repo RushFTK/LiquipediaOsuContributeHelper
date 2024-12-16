dda4e00814423b2a872222420143ec718c9c0f08
Rush_FTK <867871766@qq.com>
2024-12-16 11:12:12 +0800

    Using `.env` to save client keys and secret, instead of hardcode on files.

--- commons.py
+++ commons.py
@@ -1,11 +1,22 @@
+import os
 from ossapi import Ossapi
+from dotenv import load_dotenv
 
 client_id = None
 client_secret = None
 
 
 def generate_osu_api():
-    return Ossapi(client_id, client_secret)
+    try:
+        load_dotenv()
+        client_id = os.getenv("client_id")
+        client_secret = os.getenv("client_secret")
+        api_obj = Ossapi(client_id, client_secret)
+    except AttributeError:
+        print('please fill client_id / secret in utils/common.py before using\n'
+            + 'You can find on osu!web settings (https://osu.ppy.sh/home/account/edit)')
+        api_obj = None
+    return api_obj
 
 def clean_clan_tags(player_name: str):
     tags = ['[GB]', '[Crz]', '[Paw]', '[LS]', '[Mom]', 'ERA ', '[RS]', '[KN]', '[RUE]',
